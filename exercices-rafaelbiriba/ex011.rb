8c2bd88a90f871bb56e6ac92b35dd2fe3bc71b54
jhoaniceoliveira@gmail.com <jhoaniceoliveira@gmail.com>
2021-08-21 18:23:34 -0300

    corrigindo resposta do ex011.rb

--- exercices-rafaelbiriba/ex011.rb
+++ exercices-rafaelbiriba/ex011.rb
@@ -2,12 +2,12 @@ aleartorio = {}
 
 aleartorio[:a] = []
 aleartorio[:b] = []
-aleartorio[:c] = []
+aleartorio[:"c"] = []
 
 aleartorio[:a] << "Gandalf é melhor mago das histórias fantásticas"
 aleartorio[:b] << 102
-aleartorio[:c] << 25.2
+aleartorio[:"c"] << 25.2
 
 puts aleartorio[:a]
 puts aleartorio[:b]
-puts aleartorio[:c]
+puts aleartorio[:"c"]
